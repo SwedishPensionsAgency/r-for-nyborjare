852363edbd1be7be9ef860566f9f1f8bf1dd04e4
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-11-16 19:38:01 +0100

    upd render.r

--- render.r
+++ render.r
@@ -1,8 +1,6 @@
 library(whisker)
 library(slidify)
 
-handout <- TRUE
-
 read_file <- function(file) {
   con <- file(file)
   txt <- paste(readLines(con, warn = F), collapse = "\n")
@@ -17,10 +15,23 @@ write_file <- function(str, file) {
    close(con)
 }
 
-str <- whisker.render(
-   read_file("part-1.template"),
-   data = list(handout = handout)
-)
+render_rmd <- function(file, handout) {
+   res <- whisker.render(
+      read_file(file),
+      data = list(handout = handout)
+   )
+   return(res)
+}
 
-write_file(str, "part-1.Rmd")
-slidify("part-1.Rmd")
+file <- "part-1.template"
+
+# Render presentation
+str <- render_rmd(file, F)
+output <- gsub(".template", "-handout.html", file)
+knit2html(text = str, output =  output)
+
+# Render handout
+str <- render_rmd(file, T)
+output <- gsub(".template", ".Rmd", file)
+write_file(str, output)
+slidify(output)
